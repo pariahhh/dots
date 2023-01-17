30fc827d4776a6b78171042d38a20e16172303f4
Lemon <lemon@lemonjamesd.com>
2023-01-17 00:28:42 -0500

    Updated dot files

--- cp.fish
+++ cp.fish
@@ -3,15 +3,19 @@ set PAST $PWD
 cd ~/.dots
 
 # Eww
+rm -r ~/.dots/eww
 cp -r ~/.config/eww ~/.dots
 
 # Hyprland
+rm -r ~/.dots/hypr
 cp -r ~/.config/hypr/ ~/.dots
 
 # Helix
+rm -r ~/.dots/helix
 cp -r ~/.config/helix ~/.dots
 
 # Nixos
+rm -p ~/.dots/nixos
 cp -r /etc/nixos/ ~/.dots
 
 # Home-manager
