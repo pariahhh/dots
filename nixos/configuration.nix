524e523633deb53dc9492762235543104eda9645
LemonjamesD <lemon@lemonjamesd.com>
2023-01-20 16:32:02 -0500

    Updated dot files

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -47,23 +47,6 @@
     enableSSHSupport = true;
   };
 
-
-  #services.xserver = {
-  #  enable = true;
-
-  #  # Keyboard layout
-  #  layout = "us";
-  #  xkbVariant = "dvp";
-    
-  #  displayManager = {
-  #    lightdm.enable = true;
-  #  };
-
-  #  desktopManager = {
-  #     # plasma5.enable = true;
-  #  };
-  #};
-
   # Nvidia
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.opengl = { # this fixes the "glXChooseVisual failed" bug, context: https://github.com/NixOS/nixpkgs/issues/47932
@@ -77,16 +60,9 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "prometheus"; # Define your hostname.
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "America/New_York";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
@@ -100,9 +76,6 @@
   services.printing.enable = true;
 
   # Enable sound.
-  # sound.enable = true;
-  # hardware.pulseaudio.enable = true;
-
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -112,9 +85,6 @@
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.lemon = {
@@ -176,14 +146,7 @@
     options = "--delete-older-than 3d";
   };
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.autoUpgrade.enable = true;
-  system.stateVersion = "22.11"; # Did you read the comment?
-
+  system.stateVersion = "22.11";
 }
 
