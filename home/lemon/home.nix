6b525fd7ba10ded31f31ed4ac1a634c2eaca11db
LemonjamesD <lemon@lemonjamesd.com>
2023-06-14 11:13:33 -0400

    [SCRIPT] Updated dot files! 🚀

--- home/lemon/home.nix
+++ home/lemon/home.nix
@@ -1,7 +1,7 @@
 { config, pkgs, username, homeDirectory, stateVersion, hypr-contrib, ... }:
 
 {
-  imports = [ ./lemon.nix ];
+  imports = [ ./lemon.nix { inherit hypr-contrib; } ];
 
   # Allow unfree
   nixpkgs.config.allowUnfreePredicate = _: true;
