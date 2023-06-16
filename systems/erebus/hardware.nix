2e8872db2e48ae1c750f357de50c986e37a6d620
LemonjamesD <lemon@lemonjamesd.com>
2023-06-16 18:53:18 -0400

    [SCRIPT] Updated dot files! 🚀

---
+++ systems/erebus/hardware.nix
@@ -0,0 +1,40 @@
+{}:
+{
+  imports = [ ./hardware-configuration.nix ];
+
+  boot.loader = {
+    efi = {
+      # canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi";
+    };
+    grub = {
+      configurationLimit = 5;
+      efiSupport = true;
+      efiInstallAsRemovable = true;
+      device = "nodev";
+      extraEntries = ''
+        menuentry "Windows" {
+          insmod part_gpt
+          insmod fat
+          insmod search_fs_uuid
+          search --fs-uuid --no-floppy --set=root 8C07-315B
+          chainloader /EFI/Microsoft/Boot/bootmgfw.efi
+        }
+        menuentry "Reboot" {
+          reboot
+        }
+        menuentry "Poweroff" {
+          halt
+        }
+      '';
+    }; 
+  };
+
+  # Make some extra kernel modules available to NixOS
+  boot.extraModulePackages = with config.boot.kernelPackages;
+    [ 
+      v4l2loopback.out
+    ];
+
+  boot.supportedFilesystems = [ "ntfs" ];
+}
